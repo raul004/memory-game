ec54d0d22e5ead762ee8e871197e55749dabd6fc
Raul Vinicius <raullwx@gmail.com>
2024-02-19 12:56:52 -0400

    js: add validateInput function

--- js/login.js
+++ js/login.js
@@ -1,3 +1,18 @@
 const form = document.querySelector('.login-form');
 const input = document.querySelector('.login-input');
 const button = document.querySelector('.login-button');
+
+const validadeInput = ({target}) => {
+    
+    if(target.value.length > 3) {
+        button.classList.remove('hidden');
+        button.classList.add('show');
+        
+        button.removeAttribute('disabled'); 
+        return;
+    }
+
+    button.setAttribute('disabled', '');
+}
+
+input.addEventListener('input', validadeInput);
