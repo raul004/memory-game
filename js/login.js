5d0251212dfe381b59b59d77a980913f65a2990e
Raul Vinicius <raullwx@gmail.com>
2024-02-19 13:01:34 -0400

    js: add handleSubmit function

--- js/login.js
+++ js/login.js
@@ -7,12 +7,22 @@ const validadeInput = ({target}) => {
     if(target.value.length > 3) {
         button.classList.remove('hidden');
         button.classList.add('show');
-        
+
         button.removeAttribute('disabled'); 
         return;
     }
 
     button.setAttribute('disabled', '');
-}
+};
+
+const handeSubmit = (event) => {
+    event.preventDefault();
+
+    localStorage.setItem('player: ', input.value);
+    window.location = 'pages/game.html';
+
+    input.value = '';
+};
 
 input.addEventListener('input', validadeInput);
+form.addEventListener('submit', handeSubmit);
