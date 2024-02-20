9a00961b41e07fcb78cd615d89711869809604f5
Raul Vinicius <raullwx@gmail.com>
2024-02-20 12:07:10 -0400

    js: characters list

--- js/game.js
+++ js/game.js
@@ -1,3 +1,16 @@
 const grid = document.querySelector('.grid');
 const player = document.querySelector('.player');
 const timer = document.querySelector('.timer');
+
+const characters = [
+    'beth',
+    'jerry',
+    'jessica',
+    'morty',
+    'meeseeks',
+    'pessoa-passaro',
+    'pickle-rick',
+    'rick',
+    'scroopy',
+    'summer',
+];
